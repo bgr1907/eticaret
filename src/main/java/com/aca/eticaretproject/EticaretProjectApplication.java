89003320869ba3d06a77cbddbdffddc8a3a2ba17
Arif Koçpınar <arifkocpinar1881@hotmail.com>
2019-04-27 20:26:43 +0300

    Model Mapper Eklendi

--- src/main/java/com/aca/eticaretproject/EticaretProjectApplication.java
+++ src/main/java/com/aca/eticaretproject/EticaretProjectApplication.java
@@ -1,7 +1,10 @@
 package com.aca.eticaretproject;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EticaretProjectApplication {
@@ -10,5 +13,11 @@ public class EticaretProjectApplication {
 
 		SpringApplication.run(EticaretProjectApplication.class, args);
 	}
+	@Bean
+	public ModelMapper getModelMapper(){
 
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		return modelMapper;
+	}
 }
