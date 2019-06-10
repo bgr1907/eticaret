67754c8a1582ebe80eb048ba6e0a084889ef5557
bgr <akarabacak38@gmail.com>
2019-06-10 22:28:14 +0300

    Register eklendi.

--- src/webui/src/app/register/register.component.ts
+++ src/webui/src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {first} from "rxjs/operators";
+import {Router} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthenticationService} from "../security/authentication.service";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  loading = false;
+  submitted = false;
+  error = '';
+
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private authenticationService: AuthenticationService) {
+  }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      no: ['', Validators.required],
+      building: ['', Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      postalCode: ['',],
+      email: ['', Validators.required, Validators.email]
+
+    });
+    this.authenticationService.logout();
+  }
+
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  register() {
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.authenticationService.register(this.registerForm.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate(['/login']);
+        },
+        error => {
+          this.error = error;
+          this.loading = false;
+        });
   }
 
 }
